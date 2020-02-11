b669441f40028c5fbf60904ac3dd0a959f4f83d8
xavvvier <xavvvier@users.noreply.github.com>
2020-02-11 19:11:17 -0400

    Dropdown configuration pages

---
+++ projectTracker/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using projectTracker.Models;
+
+namespace projectTracker.Controllers
+{
+
+    [Authorize]
+    public class CategoryController : Controller {
+
+        public IActionResult Index() { 
+            return View();
+        }
+
+        public IActionResult Add() { 
+            return View("Edit");
+        }
+
+        public IActionResult Edit(int id) { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit() { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete() { 
+            return View();
+        }
+
+
+    }
+}
