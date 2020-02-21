dde11adfb0721a111d7d79ce9b94a5f81341a856
xavvvier <xavvvier@users.noreply.github.com>
2020-02-20 21:42:17 -0400

    React simple components, babel building

---
+++ projectTracker/react/hello.js
@@ -0,0 +1,12 @@
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <span>Hey span</span>
+        );
+    }
+}
