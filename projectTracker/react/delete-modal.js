eba5602e529b5fd648b5585f20eaf5495638548d
xavvvier <xavvvier@users.noreply.github.com>
2020-02-23 10:48:18 -0400

    Highlight name of item to delete

--- projectTracker/react/delete-modal.js
+++ projectTracker/react/delete-modal.js
@@ -6,7 +6,12 @@ export default class DeleteModal extends React.Component {
          <div className="ui mini test modal transition">
             <div className="header"> Delete {this.props.title} </div>
             <div className="content">
-               <p>Are you sure you want to delete {this.props.content}</p>
+                  <p>
+                      Are you sure you want to delete &nbsp; 
+                      <span className="ui label">
+                          {this.props.content}
+                      </span>
+                  </p>
             </div>
             <div className="actions">
                <div className="ui basic positive button" tabIndex="0">No</div>
