592762fc8272db9e5943cef1f3426375ad725b87
chrisfredericks <45493878+chrisfredericks@users.noreply.github.com>
2020-03-12 10:23:07 -0300

    Re-arranged menu items

--- projectTracker/wwwroot/js/site.js
+++ projectTracker/wwwroot/js/site.js
@@ -1,12 +1,3 @@
-﻿function startAutoLogout() {
-    window.setTimeout(function() {
-        // session has expired!
-        document.location = "/";
-
-    }, 1200000);
-    
-    window.setTimeout(function() {
-        // session has expired!
-        document.getElementById("lblExpire").innerHTML = "Warning : Session is about to expire!";
-    }, 1080000);
-}
+﻿$('.ui.dropdown')
+  .dropdown()
+;
